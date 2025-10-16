22a29baaf1fe0c4ae4e4f980f4bed34582cb60d3
Mariya <mawa-melnikova@ya.ru>
2025-10-16 17:37:55 +0300

    created tables

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,60 @@
+# catalog/models.py
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Category(models.Model):
+    """
+    Модель категории продуктов.
+    """
+
+    name = models.CharField(max_length=100, verbose_name="Наименование")
+    description = models.TextField(blank=True, null=True, verbose_name="Описание")
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ["name"]  # сортировка по имени
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    """
+    Модель продукта.
+    """
+
+    name = models.CharField(max_length=200, verbose_name="Наименование")
+    description = models.TextField(blank=True, null=True, verbose_name="Описание")
+    image = models.ImageField(
+        upload_to="products/images/", blank=True, null=True, verbose_name="Изображение"
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="products",
+        verbose_name="Категория",
+    )
+    price = models.DecimalField(
+        max_digits=10, decimal_places=2, verbose_name="Цена за покупку"
+    )
+    created_at = models.DateTimeField(
+        default=timezone.now, verbose_name="Дата создания"
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True, verbose_name="Дата последнего изменения"
+    )
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = [
+            "created_at",
+            "price",
+            "category",
+        ]  # сортировка по дате создания (сверху новые)
+
+    def __str__(self):
+        return self.name
--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-@mk$mn2pqy$scz0*8be(u3kwp4rg&^cxwku&u*vu!o3l3@mp7f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'catalog',
+    "catalog",
 ]
 
 MIDDLEWARE = [
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "django_project_db",
+        "USER": "postgres",
+        "PASSWORD": "qweasd963",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
